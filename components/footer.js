cfe5598787f195b2f5ed219b766945465ce86e38
Daeyeong_Park <44021486+CocoPig1028@users.noreply.github.com>
2022-08-11 22:25:48 +0900

    edit footer
    edit footer name

--- components/footer.js
+++ components/footer.js
@@ -12,14 +12,14 @@ export default function Footer() {
               </a>
             </Link>
             <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
-              © 2020 Tailblocks
+              © 2022 CocoPig1028
               <a
                 href="https://twitter.com/knyttneve"
                 rel="noopener noreferrer"
                 className="text-gray-600 ml-1"
                 target="_blank"
               >
-                @knyttneve
+                @dae_0bak
               </a>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
