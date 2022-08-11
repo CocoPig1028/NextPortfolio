69b9e12ecbc148ed57da3e241f41c81a1df9c462
Daeyeong_Park <44021486+CocoPig1028@users.noreply.github.com>
2022-08-11 19:57:50 +0900

    add contact

--- pages/about-me.js
+++ pages/about-me.js
@@ -1,7 +1,17 @@
+import Head from "next/head";
+import Layout from "../components/layout.js";
+import Contact from "../components/contact/contact.js";
+
 export default function AboutMe() {
-    return (
-        <div>
-            <h1>자기소개</h1>
-        </div>
-    );
+  return (
+    <Layout>
+      <Head>
+        <title>CocoPig 포트폴리오</title>
+        <meta name="description" content="매일매일 빡코딩" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Contact></Contact>
+    </Layout>
+  );
 }
